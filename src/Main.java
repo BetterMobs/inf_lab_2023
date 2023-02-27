d852e8e1af282ac4ef3bf00a20c8cfced974f0fd
Better Mobs <wikimal200310@gmail.com>
2023-02-27 17:39:29 +0100

    zad1b

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        Point p = new Point(1, 2);
-        System.out.println(p.x);
-        System.out.println(p.y);
+        Point p1 = new Point(0, 3);
+        Point p2 = new Point(4,0);
+        Segment s1 = new Segment(p1, p2);
+//        System.out.println(s1.getP2().x);
+        System.out.println(s1.distance());
+//        System.out.println(p1.x);
+//        System.out.println(p1.y);
     }
 }
